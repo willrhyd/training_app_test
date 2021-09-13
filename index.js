a7f074732c1269f90e7f5c870ecd60e93ef10db0
willrhyd <34925929+willrhyd@users.noreply.github.com>
2021-09-13 21:47:34 +0100

    pmc date ordering

--- index.js
+++ index.js
@@ -225,11 +225,13 @@ app.get('/pmc/:user', ensureAuthenticated, async function(req, res) {
   try {
     var rides = await Ride.find({
       user: req.user.username
-    });
-    // Assign tss average to each date CTLtoday = CTLyesterday + (TSStoday - CTLyesterday)(1/CTL time constant)
-    var data = fit.pmc(rides, )
+    }).sort({ date: 'asc'}).exec(
+      var data = fit.pmc(rides)
 
-    res.send(data)
+      res.send(data)
+    );
+    // Assign tss average to each date CTLtoday = CTLyesterday + (TSStoday - CTLyesterday)(1/CTL time constant)
+
 
   } catch (err) {
     console.log(err);
